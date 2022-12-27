d5e95b41bdc1aaed09a59e8078e7b7d4d8cad146
LaPressIII <121470191+LaPressIII@users.noreply.github.com>
2022-12-27 03:58:11 +0000

    trial

---
+++ Untitled-1.java
@@ -0,0 +1,14 @@
+
+
+/*
+Test
+
+*/
+  public class Solution {
+      public static void main(String[] args) {
+       System.out.println("Amigo is very smart");
+       String Ace = "Donald";
+       String Dancer= "Ace + Jack";
+       System.out.println(Dancer);
+}
+ }
