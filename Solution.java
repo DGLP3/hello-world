b3fe9d8ed0bd332db59c638a686d3788267ad0a7
LaPressIII <121470191+LaPressIII@users.noreply.github.com>
2022-12-27 04:18:08 +0000

    String concatenation

---
+++ Solution.java
@@ -0,0 +1,17 @@
+
+
+/*
+Test
+
+*/
+  public class Solution { 
+          public static void main(String[] args) {
+       System.out.println("Amigo is very smart");
+      String s1 = "Just ";
+      String s2 = "Do ";
+      String s3= "It";
+      System.out.println(s1+s2+s3);
+
+      
+}
+ }
