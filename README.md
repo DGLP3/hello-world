ed52937c9a443266128bf5bf8cb52602c9af3da1
LaPressIII <121470191+LaPressIII@users.noreply.github.com>
2022-12-27 04:37:28 +0000

    Hello World Java Intro

--- README.md
+++
@@ -1,7 +0,0 @@
-# hello-world
-/* Java print line pracice for Github Tutorial*/
-public class  Solution {
-  public static void main (String[] args){
-  System.out.println("hello world") ;
-}
-}
--- Solution.java
+++ Solution.java
@@ -1,16 +1,15 @@
 
 
-/*
-Test
-
-*/
+//Concatenation practice 
   public class Solution { 
           public static void main(String[] args) {
-       System.out.println("Amigo is very smart");
-      String s1 = "Just ";
-      String s2 = "Do ";
-      String s3= "It";
-      System.out.println(s1+s2+s3);
+       System.out.println("Hello World");
+      String s1 = "I'm ";
+      String s2 = " starting ";
+      String s3= "my coding ";
+      String s4= "journey";
+      System.out.println(s1+s2+s3+s4);
+
 
       
 }
