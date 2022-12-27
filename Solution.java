e9004606c7c27a1af9a5d9708c74da28f307b892
LaPressIII <121470191+LaPressIII@users.noreply.github.com>
2022-12-27 04:39:38 +0000

    Merge Hello World

--- Solution.java
+++ Solution.java
@@ -1,6 +1,6 @@
 
 
-/CodeGym Concatenation practice
+//CodeGym Concatenation practice
   public class Solution { 
           public static void main(String[] args) {
        System.out.println("Hello World");
